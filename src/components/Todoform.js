3678dd8b0f6fed631daa1fac9b9cf67da13e2119
RyanFernando27 <138043611+RyanFernando27@users.noreply.github.com>
2024-12-16 09:56:37 +0530

    implement function to get user input and display in the console (useState)

--- src/components/Todoform.js
+++ src/components/Todoform.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Todoform = () => {
+  const [value, setValues] = useState("");
+
+  /**
+   * when submit the value write in the textbox it will display .
+   * @param {event} e
+   */
+  const handelSubmit = (e) => {
+    e.preventDefault(); // to prevent page from reloading use this method
+    console.log(value);
+  };
+
   return (
-    <form className="TodoForm">
+    <form className="TodoForm" onSubmit={handelSubmit}>
       <input
         type="text"
         className="todo-input"
         placeholder="What is the task Today!!"
+        onChange={(e) => setValues(e.target.value)}
       />
       <button type="submit" className="todo-btn">
-        ADD
+        Add Task
       </button>
     </form>
   );
